4702cfca14d1df0390ac0377d95ea5e13bf467dc
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-25 11:44:40 +0000

    feat: task 1 -> add Exercise1 class

---
+++ ExceptionHandlingExercises/Exercise1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExceptionHandlingExercises
+{
+    public class Exercise1
+    {
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static void DivideUserInputs()
+        {
+            while (true)
+            {
+                try
+                {
+                    int[] input = GetUserInputs();
+                    int output = Divide(input[0], input[1]);
+
+                    Console.WriteLine($"The result is: {output}");
+                }  
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+        }
+        private static int[] GetUserInputs()
+        {
+            Console.Write("Enter divisor: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter dividend: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int[] userInputs = [num1, num2];
+
+            return userInputs;
+        }
+    }
+}
